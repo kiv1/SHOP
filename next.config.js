5646a2cf253fa81fd05a4c689fefa928413cf489
kiv1 <zaki.jst@gmail.com>
2024-04-01 15:47:14 +0800

    Remove cache

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  cacheMaxMemorySize: 0, // disable default in-memory caching
   images: {
     domains: ["lh3.googleusercontent.com", "vercel.com"],
   },
