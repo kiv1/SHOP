4ae4bccc4726173555438f4541f9a63ee5d9db32
kiv1 <zaki.jst@gmail.com>
2024-04-01 16:16:34 +0800

    add 1 sec  cache

--- components/home/shop.tsx
+++ components/home/shop.tsx
@@ -15,7 +15,7 @@ export default function Shop({ session }: { session: Session | null }) {
       headers: {
         "Content-Type": "application/json",
       },
-      next: { revalidate: 10 },
+      next: { revalidate: 1 },
     });
     let data = await result.json();
     setValue(data.allItems);
